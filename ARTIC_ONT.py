3c263e23086e2fb58ea25d734e5b2da4de695d48
HadrienRegue <hadrien.regue@univ-lyon1.fr>
2020-08-28 12:22:11 +0200

    maj

--- ARTIC_ONT.py
+++ ARTIC_ONT.py
@@ -3,11 +3,16 @@ import glob
 
 configfile : "config/config.yaml"
 
-datapath=config['PathToData']
 BC_path=config['BARCODEpath']
+if (BC_path[-1] != "/"):
+	BC_path=BC_path+"/"
+
+results_path=config['RESULTpath']
+if (results_path[-1] != "/"):
+	results_path=results_path+"/"
 
 #get all barcodes in a list after demultiplexing
-barcode_list = glob.glob(datapath+"barcode*")
+barcode_list = glob.glob(BC_path+"barcode*")
 BARCODE=[]
 for BC in barcode_list:
 	barcode=str(os.path.basename(BC))
@@ -15,20 +20,20 @@ for BC in barcode_list:
 
 rule pipeline_output:
     input:
-        filter_fastq = expand(resultpath+"FILTER/{barcode}/merged.fastq"  ,barcode=BARCODE),
-
+        filter_fastq = expand(results_path+"FILTER/{barcode}/merged.fastq"  ,barcode=BARCODE),
 
 rule filter:
     message:
         "filter fastq."
     input:
-        lambda wildcards: expand(resultpath+"barcoding/"+"{barcode}", barcode=BARCODE)
+        #lambda wildcards: expand(BC_path+"{barcode}", barcode=BARCODE) {params.path}
+        BC_path+"{barcode}"
     output:
-        filter_fastq = resultpath+"FILTER/{barcode}/merged.fastq" 
+        filter_fastq = results_path+"FILTER/{barcode}/merged.fastq" 
     params:
-        path = resultpath+"barcoding/"+"{barcode}"       
+        path = BC_path+"{barcode}"     
     shell:
         """
             artic guppyplex --skip-quality-check --min-length 400 --max-length 700 \
-                --directory {params.path} --prefix run_name --output {output}
+                --directory {input} --prefix run_name --output {output}
         """
--- ARTIC_ONT_launch.sh
+++ ARTIC_ONT_launch.sh
@@ -2,9 +2,10 @@
 
 
 path_barcoding="/srv/nfs/ngs-stockage/NGS_Virologie/CGINEVRA/Artic_Minion/2020-07-03_Run_5_ARTIC/2020-07-03_Run_5_ARTIC/barcoding"
-
+output_dir="/srv/nfs/ngs-stockage/NGS_Virologie/CGINEVRA/Artic_Minion/2020-07-03_Run_5_ARTIC/2020-07-03_Run_5_ARTIC/ARTIC_RESULTS"
 
 snakemake -s ARTIC_ONT.py \
-    --core 10 \
+    --core 1 \
     --config \
-        BARCODEpath=$path_barcoding
+        BARCODEpath=$path_barcoding \
+        RESULTpath=$output_dir
